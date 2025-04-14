9bc70c608d3c1ee9cf5945512298a57e1dd753db
Pen-tax <161380400+Pen-tax@users.noreply.github.com>
2025-04-14 04:27:03 +0100

    new improvements
    added new functionality to allow the user to choose the name of the text file to save to as well as a new function that allows the user to add multiple entries back to back without having to restart the program

--- UserSystem.py
+++ UserSystem.py
@@ -2,30 +2,46 @@
 
 ## function to add user email and gamertag
 def userinfo():
-  filename = "Userinfo.txt"
-  textfile = open(filename,"w")
-  realemail = False
-  while realemail == False:
-    email = input("Please Enter Your E-Mail Address\nEnter Here: ")
-    if email in filename:
-      print("\nE-Mail Address already in System!\n")
-    else:
-      realemail == True
-      textfile.write(email+" : ")
-      break
-  realgamer = False
-  while realgamer == False:
-    gamertag = input("Please Enter Your Gamertag\nEnter Here: ")
-    if gamertag in filename:
-      print("\nPlease Enter a Different Gamertag!\n")
-    else:
-      realgamer == True
-      textfile.write(gamertag+"\n")
-      break    
-  textfile.close()
+    file = input("Please Enter the File Name\nEnter Here: ")
+    filename = file + ".txt"
+    print("Contents will be Written to " + filename + "\n")
+    writefile(filename)
 
+def writefile(filename):
+    textfile = open(filename, "w")
+    running = True
+    while running == True:
+        try:
+            realemail = False
+            while realemail == False:
+                email = input("Please Enter Your E-Mail Address\nEnter Here: ")
+                if email in filename:
+                    print("\nE-Mail Address already in System!\n")
+                else:
+                    realemail = True  # Fixed assignment operator
+                    textfile.write(email + " : ")
+                    break
+            realgamer = False
+            while realgamer == False:
+                gamertag = input("Please Enter Your UserName\nEnter Here: ")
+                if gamertag in filename:
+                    print("\nUsername is already in Use\n")
+                else:
+                    realgamer = True  # Fixed assignment operator
+                    textfile.write(gamertag + "\n")
+                    break
+            choice = input("Would you like to add another user? (Y/N)\nEnter Here: ")
+            if choice.lower() == "y":
+                running = True
+            elif choice.lower() == "n":
+                running = False
+                print("Exiting Program and closing file...")
+                textfile.close()
+            else:
+                print("Invalid Input")
+        except IOError:
+            print("File not found")
 
-  ## main
+## main
 userinfo()
-  
-  
+
